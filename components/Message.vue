d4627786fc1cd72d40c4f90c600803c09770b66e
J.B Stepan <tonymoooon543@gmail.com>
2022-07-04 17:23:30 -0500

    Initial Commit

---
+++ components/Message.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+defineProps({
+    content: String,
+    user: String
+})
+</script>
+
+<template>
+    <div>
+        <p>{{content}}</p>
+        <p>Sent by: {{user}}</p>
+    </div>
+</template>
+
+<style>
+div {
+    position: relative;
+    width: 200px;
+    border: 1px solid black;
+}
+
+p {
+    word-wrap: normal;
+    font: 500;
+    font-family: sans-serif;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+useHead({
+  title: 'Home',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8'
+})
+
+const { pending, data: messages } = await useLazyFetch('http://localhost:5000/974762032832323634/messages')
+
+watch(messages, (newMessages) => {
+
+})
+
+</script>
+
+<template>
+  <div v-if="pending">
+    Loading ...
+  </div>
+  <div class="main" v-else>
+    <div v-for="message in messages" :key="message.id">
+        <div v-for="archive in message.archive" :key="archive.id">
+          <Message :content=archive.content :user=archive.user />
+        </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  #main {
+    display: flex;
+    justify-content: center;
+  }
+</style>
